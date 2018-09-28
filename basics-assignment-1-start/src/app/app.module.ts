cb81bb1cc06ff60c26c14008c91c18a044651f24
Tarik En Nakdi <tarik.ennakdi@kaiser-x.com>
2018-09-28 11:12:01 +0200

    assignment 1

--- basics-assignment-1-start/src/app/app.module.ts
+++ basics-assignment-1-start/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CommentsComponent } from './comments/comments.component';
+import { WarningMessageComponent } from './warning-message/warning-message.component';
+import { SuccessMessageComponent } from './success-message/success-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentsComponent,
+    WarningMessageComponent,
+    SuccessMessageComponent
   ],
   imports: [
     BrowserModule,
