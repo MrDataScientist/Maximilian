42411e3aa331f8335ebaece2edca2a2b98a61ebe
Tarik En Nakdi <tarik.ennakdi@kaiser-x.com>
2018-10-12 11:16:30 +0200

    updates

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  allowNewServer = true;
+  serverCreationStatus = 'test';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServer(event: any) {
+    console.log(event);
+  }
 }
