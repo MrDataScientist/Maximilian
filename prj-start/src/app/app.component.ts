0a020a6a7c2a5d4421d0a46e51947f059f05bf00
Tarik En Nakdi <tarik.ennakdi@kaiser-x.com>
2018-10-16 10:23:24 +0200

    update

--- prj-start/src/app/app.component.ts
+++ prj-start/src/app/app.component.ts
@@ -6,12 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showSecret = false; // property
-  log = [];
-
-  onToggleDetails() {
-    this.showSecret = !this.showSecret;
-    //this.log.push(this.log.length + 1);
-    this.log.push(new Date());
-  }
 }
