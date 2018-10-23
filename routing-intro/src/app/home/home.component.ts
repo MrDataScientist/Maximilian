4e9450c59f6da4dc80d349f96aacba284115c242
Tarik En Nakdi <tarik.ennakdi@kaiser-x.com>
2018-10-23 15:30:53 +0200

    contact form

--- routing-intro/src/app/home/home.component.ts
+++ routing-intro/src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  topics = ['Angular', 'React', 'Vue'];
+
   constructor() { }
 
   ngOnInit() {
