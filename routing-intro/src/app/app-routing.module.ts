72047e1a36aa4901e90cb8985bc56d6e46d175c0
Tarik En Nakdi <tarik.ennakdi@kaiser-x.com>
2018-10-23 14:35:05 +0200

    routing

--- routing-intro/src/app/app-routing.module.ts
+++ routing-intro/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {SettingsComponent} from './settings/settings.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {IndexComponent} from './index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'index', component: IndexComponent },
+  {path: 'home', component: HomeComponent },
+  {path: 'settings', component: SettingsComponent },
+  {path: '**', component: PageNotFoundComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- routing-intro/src/app/app.module.ts
+++ routing-intro/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { SettingsComponent } from './settings/settings.component';
+import { HomeComponent } from './home/home.component';
+import { IndexComponent } from './index/index.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    HomeComponent,
+    IndexComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
